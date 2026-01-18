7ba598d7b82e7012c11a7a5fba2caba7f9a14e2d
Lucas Franco Rocha <franco100lucas@gmail.com>
2026-01-18 17:44:58 -0300

    Fix: Remove uncommented phrase

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    namespace :v1 do para incluir formato JSON padrã para incluir formato JSON padrãoo
+    namespace :v1 do
       # Here we will put all our API routes
     end
   end
