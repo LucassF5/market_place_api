a3880ee760cb9b1cdaa3e37de1009f2e4e4200e4
Lucas Franco Rocha <franco100lucas@gmail.com>
2026-01-19 23:20:32 -0300

    Feat: Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      # Here we will put all our API routes
+      resources :users, only: [:show]
     end
   end
 
