73d3e5c35aac257c2a9bedcb6083aff153b193e9
Lucas Franco Rocha <franco100lucas@gmail.com>
2026-01-20 21:54:54 -0300

    Feat: Adds update action to the users controller

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -4,26 +4,36 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
-  
+
   test "should show user" do
     get api_v1_user_url(@user), as: :json
     assert_response :success
 
     json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['email']
+    assert_equal @user.email, json_response["email"]
   end
 
   test "should create user" do
     assert_difference("User.count") do
-      post api_v1_users_url, params: { user: {email: "teste@teste.com", password: "password"} }, as: :json
+      post api_v1_users_url, params: { user: { email: "teste@teste.com", password: "password" } }, as: :json
     end
     assert_response :created
   end
 
   test "should not create user with taken email" do
     assert_no_difference("User.count") do
-      post api_v1_users_url, params: { user: {email: @user.email, password: "123345576" }}, as: :json
+      post api_v1_users_url, params: { user: { email: @user.email, password: "123345576" } }, as: :json
     end
+    assert_response :unprocessable_entity
+  end
+
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: "1234576" } }, as: :json
+    assert_response :success
+  end
+
+  test "should not update user with taken email" do
+    patch api_v1_user_url(@user), params: { user: { email: "bad_email", password: "1234576" } }, as: :json
     assert_response :unprocessable_entity
   end
 end
