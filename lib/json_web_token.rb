ce1bd48ca70f2a2c42d7cbdb34d86a9353aa84bd
Lucas Franco Rocha <franco100lucas@gmail.com>
2026-01-22 12:44:33 -0300

    Feat: Adds JWT encriptation methods to initialize

---
+++ lib/json_web_token.rb
@@ -0,0 +1,13 @@
+class JsonWebToken
+    RAILS_SECRET_KEY = Rails.application.credentials.secret_key_base
+
+    def self.encode(payload, exp = 24.hours.from_now)
+        payload[:exp] = exp.to_i
+        JWT.encode(payload, RAILS_SECRET_KEY)
+    end
+
+    def self.decode(token)
+        decoded = JWT.decode(token, RAILS_SECRET_KEY).first
+        HashWithIndifferentAccess.new decoded
+    end
+end
