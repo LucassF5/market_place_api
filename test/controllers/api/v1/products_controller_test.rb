be97e36dfdb369b022fb8c2f3df46cff4e986d66
Lucas Franco Rocha <franco100lucas@gmail.com>
2026-02-03 07:07:50 -0300

    WIP: Adds create action

---
+++ test/controllers/api/v1/products_controller_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+
+class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @product = products(:one)
+  end
+
+  test "should show the products" do
+    get api_v1_products_url(), as: :json
+    assert_response :success
+  end
+
+  test "should show the product" do
+    get api_v1_product_url(@product), as: :json
+    assert_response :success
+
+    json_response = JSON.parse(self.response.body)
+    assert_equal @product.title, json_response["title"]
+  end
+
+  test "should create product" do
+    assert_difference("Product.count") do
+      post api_v1_products_url,
+        params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+        headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+        as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should forbid create product" do
+    assert_no_difference("Product.count") do
+      post api_v1_products_url,
+        params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+        as: :json
+    end
+    assert_response :forbidden
+  end
+end
