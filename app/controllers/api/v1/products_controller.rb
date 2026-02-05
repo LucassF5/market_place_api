be2219b52cdea12f7b06572a2e25f952b24ec9db
Lucas Franco Rocha <franco100lucas@gmail.com>
2026-02-05 02:16:57 -0300

    Feat: Adds the products create, update and destroy actions

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
-    before_action :set_product, only: [ :show ]
+    before_action :set_product, only: [ :show, :update, :destroy ]
+    before_action :check_login, only: [ :create ]
+    before_action :check_owner, only: [ :update, :destroy ]
 
     def index
         render json: Product.all
@@ -9,9 +11,39 @@ class Api::V1::ProductsController < ApplicationController
         render json: @product
     end
 
+    def create
+        product = current_user.products.build(product_params)
+        if product.save
+            render json: product, status: :created
+        else
+            render json: { errors: product.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @product.update(product_params)
+            render json: @product, status: :ok
+        else
+            render json: @product.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @product.destroy
+        head :no_content
+    end
+
     private
 
     def set_product
         @product = Product.find(params[:id])
+    end
+
+    def product_params
+        params.require(:product).permit(:title, :price, :published)
+    end
+
+    def check_owner
+        head :forbidden unless @product.user_id == current_user&.id
     end
 end
